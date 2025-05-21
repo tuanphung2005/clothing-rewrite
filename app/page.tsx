94150c26fc1b5c6c65cffbc5af68a9f6319d5ecc
yhari <tuan29102005@gmail.com>
2025-05-21 22:31:34 +0700

    filter section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+
+import { BigGenderFilter } from "@/components/main_page/BigGenderFilter";
+import { SmallFilter } from "@/components/main_page/SmallFilter";
+import { Card } from "@heroui/card";
+
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <h1>this is clothing website</h1>
-    </section>
+    <>
+      {/* top section */}
+      <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 border border-default-200">
+        {/* gender filter buttons (all, female, male, female(kid), male(kid)) */}
+        <BigGenderFilter />
+        
+        
+      </div>
+      {/* main content */}
+      <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 border border-default-200">
+        {/* second filter for sizes, prices on the left side, and sort by on the right side */}
+        <SmallFilter />
+        {/* product card */}
+
+
+      </div>
+
+    </>
   );
 }
---
+++ components/main_page/SmallFilter.tsx
@@ -0,0 +1,62 @@
+
+import { Slider } from "@heroui/slider";
+import { Select, SelectItem } from "@heroui/select";
+
+
+export const SmallFilter = () => {
+
+    const sizes = [
+        { label: "S", value: "s" },
+        { label: "M", value: "m" },
+        { label: "L", value: "l" },
+        { label: "XL", value: "xl" },
+        { label: "XXL", value: "xxl" },
+    ]
+
+    return (
+        <div className="flex flex-col md:flex-row justify-between gap-8 w-full">
+            <div className="flex flex-row gap-4 w-full md:w-2/5">
+                <div className="flex flex-col w-full">
+                    <Slider 
+                        formatOptions={{style: "currency", currency: "VND"}}
+                        step={50000}
+                        minValue={0}
+                        maxValue={650000}
+                        defaultValue={[40000, 500000]}
+                        showTooltip
+                        size="md"
+                        className="w-full"
+                        color="foreground"
+                        label="Khoảng giá"
+                    />
+                </div>
+                <Select
+                    selectionMode="multiple"
+                    labelPlacement="inside"
+                    label="Kích cỡ"
+                    size="md"
+                    variant="bordered"
+                    className="w-1/4"
+                >
+                    {sizes.map((item) => (
+                        <SelectItem key={item.value}>
+                            {item.label}
+                        </SelectItem>
+                    ))}
+                </Select>
+            </div>
+                <div className="w-full md:w-1/4">
+                    <Select
+                        labelPlacement="inside"
+                        label="Sắp xếp theo"
+                        size="md"
+                        variant="bordered"
+                    >
+                        <SelectItem key="new">Mới nhất</SelectItem>
+                        <SelectItem key="price-asc">Giá: thấp đến cao</SelectItem>
+                        <SelectItem key="price-desc">Giá: cao đến thấp</SelectItem>
+                    </Select>
+                </div>
+      </div>
+    )
+}
